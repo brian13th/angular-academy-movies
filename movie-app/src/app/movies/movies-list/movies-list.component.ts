3ef0059490d453825fd3f72e1f0673cd86df59dc
brian13th <brian13th13@gmail.com>
2020-09-29 01:47:37 +0300

    display movies from service-movies

--- movie-app/src/app/movies/movies-list/movies-list.component.ts
+++ movie-app/src/app/movies/movies-list/movies-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DbService } from 'src/app/services/db.service';
 
 @Component({
   selector: 'app-movies-list',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies-list.component.css']
 })
 export class MoviesListComponent implements OnInit {
-
-  constructor() { }
+  movies: any[];
+  constructor(private db:DbService) {
+    this.movies = db.getAllMovies()
+   }
 
   ngOnInit(): void {
   }
