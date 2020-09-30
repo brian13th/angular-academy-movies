af8370ab565d253d1759196681c7c698d136b52d
brian13th <brian13th13@gmail.com>
2020-10-01 01:42:46 +0300

    completed paths for consts routes: Routes

--- movie-app/src/app/app-routing.module.ts
+++ movie-app/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { AppComponent } from './app.component';
 import { MovieFeatureComponent } from './movies/movie-feature/movie-feature.component';
 import { MoviesListComponent } from './movies/movies-list/movies-list.component';
 
 const routes: Routes = [
   {path: 'movies', component: MoviesListComponent},
-  {path: 'featured', component: MovieFeatureComponent}
+  {path: 'featured', component: MovieFeatureComponent},
+  {path: '', component: AppComponent}, // we can use redirectTo: 'the-path-of-the-component', pathMatch: 'full'
+  {path: "**", component: AppComponent} // we could have a custom 404Component
 ];
 
 @NgModule({
